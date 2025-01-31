7493cb27e3aa74c0713a389cedc7f3f819284d09
jasir-pv <jasirahsanpv40@gmail.com>
2025-01-31 21:52:33 +0530

    home, profile, rides and chats pages only added

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from "react-native"
+
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text> prof  </Text>
+    </SafeAreaView>
+  )
+}
+
+export default Profile
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout() {
 
   return (
       <Stack>
-        <Stack.Screen name="(index)" options={{ headerShown: false }} />
+        <Stack.Screen name="(index)" />
         <Stack.Screen name="+not-found" />
       </Stack>
   );
