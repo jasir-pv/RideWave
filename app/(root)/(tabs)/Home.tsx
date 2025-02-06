c111288c12aaacba74c64c3a7c54f1b41fa18c7a
jasir-pv <jasirahsanpv40@gmail.com>
2025-02-06 23:25:39 +0530

    feat: listEmptyComponent added (loading)

--- app/(root)/(tabs)/Home.tsx
+++ app/(root)/(tabs)/Home.tsx
@@ -1,7 +1,8 @@
 import RideCard from '@/components/RideCard'
+import { images } from '@/constants'
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
-import { FlatList, Text, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 
@@ -106,18 +107,33 @@ const recentRides = [
 
 export default function Page() {
   const { user } = useUser()
+  const loading = true
 
   return (
     <SafeAreaView className='bg-general-500'>
     <FlatList 
-      data={ recentRides?.slice(0,5) }
+      data={ recentRides?.slice(0,5)}
       renderItem={({ item }) => <RideCard ride={item} />}
       className='px-5'
       keyboardShouldPersistTaps="handled"
       contentContainerStyle= {{
         paddingBottom: 100,
       }}
-
+      ListEmptyComponent={() => (
+        <View className='flex flex-col items-center justify-center'>
+          { !loading ? (
+            <>
+              <Image source={images.noResult} className='w-40 h-40'
+              alt='No recent rides found'
+              resizeMode='contain'/>
+              <Text className='text-sm'>No recent rides found</Text>
+            </>
+          ): (
+            <ActivityIndicator size='small' color="#000" />
+          ) }
+        </View>
+      )}
+      
     />
     </SafeAreaView>
   )
