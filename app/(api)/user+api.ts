c125f2cbb3a061eb9a0a2d9c9fb5e40e28d18546
jasir-pv <jasirahsanpv40@gmail.com>
2025-02-14 23:23:51 +0530

    :feat: stripe implementation

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,45 +1,34 @@
-import { neon } from '@neondatabase/serverless';
+import { neon } from "@neondatabase/serverless";
 
-
-export async function POST( request: Request) {
-    try {
-
+export async function POST(request: Request) {
+  try {
     const sql = neon(`${process.env.DATABASE_URL}`);
-    const { name, email, clerkId } = await request.json()
+    const { name, email, clerkId } = await request.json();
 
-
-    if( !name || !email || !clerkId) {
-        return Response.json(
-            {error: "Missing required fields"},
-            { status: 400 }
-        )
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
     }
-
-
 
     const response = await sql`
-    
-    INSERT INTO users (
+      INSERT INTO users (
         name, 
-        email,
-        clerkId
-    )
-    VALUES (
-        ${name},
+        email, 
+        clerk_id
+      ) 
+      VALUES (
+        ${name}, 
         ${email},
-        ${clerkId},
-    )
+        ${clerkId}
+     );`;
 
-        `;
-
-        return new Response(JSON.stringify( {data: response}), { status: 201 })
-
-        
-    } catch (error) {
-        console.log(error)
-        return Response.json( { error: error }, { status: 500 })
-    }
+    return new Response(JSON.stringify({ data: response }), {
+      status: 201,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
-
-// See https://neon.tech/docs/serverless/serverless-driver
-// for more information
